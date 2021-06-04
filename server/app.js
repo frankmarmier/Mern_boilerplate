ac19e4734df9b31577f74d87b372f770633eeba7
1testsample <frankomarmier@gmail.com>
2021-06-04 16:44:36 +0200

    Fix #3

--- server/app.js
+++ server/app.js
@@ -52,7 +52,7 @@ app.use("/api/auth", authRouter);
 app.use((req, res, next) => {
   const error = new Error("Ressource not found.");
   error.status = 404;
-  next(err);
+  next(error);
 });
 
 // Error handler middleware
