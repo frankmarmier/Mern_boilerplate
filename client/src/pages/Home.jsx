68f14987f4b7ad2979617e3bede7bb4c6cc17a41
Stephane Mbesse <stephane.mbesse@gmail.com>
2021-06-08 16:23:46 +0200

    Alumni icon on map and dedicated card on clic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,9 +8,8 @@ import ReactMapboxGl, {
 import "mapbox-gl/dist/mapbox-gl.css";
 import axios from "axios";
 import SearchBar from "../components/SearchBar";
-import ListAlumni from "../components/ListAlumni";
 import QpvsData from "../qpv.json";
-import { useState } from "react";
+import AlumniDisplay from "../components/AlumniDisplay";
 
 // console.log(process.env.REACT_APP_MAPBOX_TOKEN)
 const Map = ReactMapboxGl({
@@ -22,12 +21,28 @@ class Home extends React.Component {
     alumnis: [],
     lng: "", // Default lng and lat set to the center of paris.
     lat: "",
+    clickedAlumni:null,
   };
 
 
-  handleClick = (selectedItem) => {
-    // WHO THE PARENT 
-    this.props.handleSelectItem(selectedItem);
+  handleClose = () => {
+    this.setState({ clickedAlumni: null });
+  };
+
+  handleClick = (event) => {
+    const imgId = event.target.id
+    console.log(imgId)
+    axios
+      .get(process.env.REACT_APP_BACKEND_URL + "/api/alumni/" + imgId)
+      .then((foundAlumni) => {
+        console.log(foundAlumni.data);
+        this.setState({ clickedAlumni: foundAlumni.data });
+        console.log(this.state.clickedAlumni);
+
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
 
@@ -44,6 +59,7 @@ class Home extends React.Component {
   }
 
   render() {
+    console.log(this.state.clickedAlumni);
     console.log(this.state.alumnis);
     return (
       <div>
@@ -71,7 +87,6 @@ class Home extends React.Component {
           </ul>
         </div>
 
-        <ListAlumni />
         <Map
           center={[2.333333, 48.866667]}
           zoom={[14]}
@@ -89,6 +104,7 @@ class Home extends React.Component {
             !alumni.locationUser.coordinates[1] ?
               <Marker
                 key={alumni._id}
+                onClick={(event) => this.handleClick(event)}
                 coordinates={[
                   2.2,
                   48.93
@@ -97,14 +113,18 @@ class Home extends React.Component {
               >
                 <img
                   src={alumni.image}
+                  id={alumni._id}
+                  // onClick={(event) => this.handleClick(event)}
                   alt="alumni"
                   style={{
                     width: 70,
                     height: 70,
+                    // markerOffset:2em;
                   }}
                 />
               </Marker> : <Marker
                 key={alumni._id}
+                onClick={(event) => this.handleClick(event)}
                 coordinates={[alumni.locationUser.coordinates[0],
                   alumni.locationUser.coordinates[1]
                 ]}
@@ -112,15 +132,24 @@ class Home extends React.Component {
               >
                 <img
                   src={alumni.image}
+                  id={alumni._id}
+                  // onClick={(event) => this.handleClick(event)}
                   alt="alumni"
                   style={{
                     width: 70,
                     height: 70,
+                    // markerOffset:2em;
                   }}
                 />
               </Marker>
           })}
 
+
+{this.state.clickedAlumni && 
+          <AlumniDisplay
+            item={this.state.clickedAlumni}
+            handleClose={this.handleClose}
+          />}
         </Map>
       </div>
     );
