27442b44b857f0ae9a16f0d361b7f898fd5122e3
Stephane Mbesse <stephane.mbesse@gmail.com>
2021-06-11 09:00:38 +0200

    Alumni list ok

--- client/src/components/AlumniList.jsx
+++ client/src/components/AlumniList.jsx
@@ -2,9 +2,9 @@ import React, { Component, Link } from "react";
 
 class AlumniList extends Component {
   handleClick = (alumni_id) => {
-    console.log(alumni_id)
-    this.props.handleCard(alumni_id)
-  }
+    console.log(alumni_id);
+    this.props.handleCard(alumni_id);
+  };
   render() {
     const filteredAlumnis = this.props.alumnis.filter((alumni) => {
       return this.props.searchValue && alumni.city === this.props.searchValue;
@@ -16,27 +16,66 @@ class AlumniList extends Component {
             filteredAlumnis.map((alumni) => {
               return (
                 <div key={alumni._id} className="profilCard">
-
-                <li className="listedAlumni">
-                  <p> {alumni.firstName} {alumni.lastName} | {alumni.industry} | <span style={{cursor: "pointer"}} onClick={() => {this.handleClick(alumni._id)}}>👁</span></p>
-                  <p>{alumni.work} {alumni.studies}</p>
-                  <p>{alumni.hood}</p>
-                  
-                </li>
-            </div>
+                  <div className="imgDiv">
+                    <img
+                      src={alumni.image}
+                      alt={alumni.firstName + alumni.lastName + " picture"}
+                    />
+                  </div>
+                  <li className="listedAlumni">
+                    <p>
+                      {" "}
+                      {alumni.firstName} {alumni.lastName} ||{" "}
+                      <i class="fa fa-building-o" aria-hidden="true"></i>
+                      {alumni.industry} |{" "}
+                      <span
+                        style={{ cursor: "pointer" }}
+                        onClick={() => {
+                          this.handleClick(alumni._id);
+                        }}
+                      >
+                        👁
+                      </span>
+                    </p>
+                    <p>
+                      {alumni.work} {alumni.studies}
+                    </p>
+                    <p>{alumni.hood}</p>
+                  </li>
+                </div>
               );
             })}
 
           {(!this.props.searchValue || this.props.searchValue.value === "") &&
             this.props.alumnis.map((alumni) => {
               return (
-                <div key={alumni._id}>
-                <li className="listedAlumni">
-                <p> {alumni.firstName} {alumni.lastName} | {alumni.industry} | <span style={{cursor: "pointer"}} onClick={() => {this.handleClick(alumni._id)}}>👁</span></p>
-                <p>{alumni.work} {alumni.studies}</p>
-                <p>{alumni.hood}</p>
-                </li>
-            </div>
+                <div key={alumni._id} className="profilCard" style={{ cursor: "pointer" }} onClick={() => {
+                          this.handleClick(alumni._id);
+                        }}>
+                  <div className="imgDiv">
+                    <img
+                      src={alumni.image}
+                      alt={alumni.firstName + alumni.lastName + " picture"}
+                    />
+                  </div>
+                  <li className="listedAlumni" >
+                    <p>
+                      {" "}
+                      <span className="firstLastName purple">
+                        {alumni.firstName} {alumni.lastName}
+                      </span>{" "}
+                      <i class="fa fa-building-o green" aria-hidden="true"></i>{" "}
+                      <span className="alumniHood">({alumni.neighborhood}) </span>
+                      {/* |{" "} <span > 👁 </span> */}
+                    </p>
+                    <p>
+                      <i className="fas fa-briefcase green"></i>
+                      <span className="alumniWork">{alumni.work}</span>{" "}
+                      <i class="fa fa-university green" aria-hidden="true"></i>
+                      <span className="alumniStudies">{alumni.studies}</span>
+                    </p>
+                  </li>
+                </div>
               );
             })}
         </ul>
